53a8b15c46d59749903c1b8101136e5811fb0e0b
Someshtiwari532@gmail.com <someshtiwari.in@gmail.com>
2026-02-22 10:51:16 +0530

    Fix: Route parameters typo

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Serve the frontend
-app.get('/', (res, send) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
